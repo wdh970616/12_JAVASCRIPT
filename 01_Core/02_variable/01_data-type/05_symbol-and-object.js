1f938850464594cfa6977add1fae78f9892ad355
wdh970616 <wdh970616@naver.com>
2024-07-24 11:23:10 +0900

    data type

---
+++ 01_Core/02_variable/01_data-type/05_symbol-and-object.js
@@ -0,0 +1,11 @@
+// 이름이 충돌할 위험이 없는 값인 심벌을 프로퍼티 키로 사용한다.
+
+// symbol
+obj[key] = 'value';
+console.log(obj[key]);
+
+/* 
+객체 타입
+자바스크립트의 데이터 타입은 원시타입(number, string, boolean, undefined, null, symbol)
+이 의외의 모든 값은 객체 타입 이라고한다. (객체, 함수, 배열, ...)
+*/
